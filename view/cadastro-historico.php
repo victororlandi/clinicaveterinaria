df7636fc303d53e9e6d443f19daf9bd55153fce4
victororlandi <victororlandi2016@gmail.com>
2025-05-28 10:47:11 -0300

    Formulários dos cadastros + imagens

--- view/cadastro-historico.php
+++ view/cadastro-historico.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
--- view/cadastro-talentos.php
+++ view/cadastro-talentos.php
@@ -2,12 +2,41 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <pre>
+    <header>
+        <section class="marca"> 
+            <img src="../assets/img/icons/logo-2.png">
+            <h1>petisco</h1>
+        </section>
+        <nav>
+            <ul>
+                <li><a href="#">A clínica</a></li>
+                <li><a href="#">Planos</a></li>
+                <li><a href="#">Nossos profissionais</a></li>
+                <li><a href="#" class="item-menu-moldura">Participar</a></li>
+                <li><a href="#" class="item-menu-moldura">Cadastro/Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="formulario-img-lateral">
+        <img id="cad-talentos" src="../assets/img/illustrations/JPG/27.jpg">
+        <section class="formulario">
+            <h1>CADASTRO DE TALENTOS</h1>
+            <form>
+                <input type="text" placeholder="Nome Completo">
+                <input type="text" placeholder="CPF">
+                <input type="text" placeholder="Área de Interesse">
+                <input type="text" placeholder="Celular">
+                <input type="text" placeholder="Endereço">
+                <input type="email" placeholder="E-mail">
+            </form>
+        </section>
+    </section>
+    <!--<pre>
         Campos:
         
         Nome completo
@@ -18,6 +47,33 @@
         Celular
         Email
         Botão para envio de currículo
-    </pre>
+    </pre> !-->
+    <footer>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3695.6348838151825!2d-51.386619625905276!3d-22.139892510911558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9493f501254620eb%3A0x16bc508109c64255!2sFatec%20de%20Presidente%20Prudente!5e0!3m2!1spt-BR!2sbr!4v1747918682634!5m2!1spt-BR!2sbr" width="300px" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <ul>
+            <li><a href="#">Nossos Serviços</a></li>
+            <li><a href="#">Dicas Para Teu Pet</a></li>
+            <li><a href="#">Presenteie com Petisco</a></li>
+            <li><a href="#">Planos</a></li>
+            <li><a href="#">Eventos</a></li>
+            <li><a href="#">Link pro zap web</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Perguntas Frequentes</a></li>
+            <li><a href="#">Política de Privacidade</a></li>
+            <li><a href="#">Termos e Condições</a></li>
+            <li><a href="#">Clube de Membros</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Nossos Contatos</a></li>
+            <section class="social">
+            <a href="#"><img src="../assets/img/icons/facebook.png"></a>
+            <a href="#"><img src="../assets/img/icons/instagram.png"></a>
+            <a href="#"><img src="../assets/img/icons/zapefron.png"></a>
+            </section>
+            <li><a href="#">Desinscreva-se</a></li>
+        </ul>
+        <img id="dog" src="../assets/img/illustrations/PNG/08-cut.png">
+    </footer>
 </body>
 </html>
